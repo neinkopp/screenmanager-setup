73f993e1def3ad3ae8b9f4d8803fd69557f6d666
neinkopp <jakob.schleitzer@gmail.com>
2020-12-01 21:53:10 +0100

    testing

--- scripts/tv_cmds.py
+++ scripts/tv_cmds.py
@@ -2,12 +2,13 @@ def tvCommand(command="toggle"):
 	import cec
 	cec.init()
 	tv = cec.Device(cec.CECDEVICE_TV)
-	if command == "power_on":
-		return tv.power_on()
-	elif command == "standby":
-		return tv.standby()
-	elif command == "toggle":
-		if tv.is_on():
-			return tv.standby()
-		else:
-			return tv.power_on()
+	# if command == "power_on":
+	# 	return tv.power_on()
+	# elif command == "standby":
+	# 	return tv.standby()
+	# elif command == "toggle":
+	# 	if tv.is_on():
+	# 		return tv.standby()
+	# 	else:
+	# 		return tv.power_on()
+	return cec.list_devices()
